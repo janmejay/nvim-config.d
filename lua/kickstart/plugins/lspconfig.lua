7b83572eae93fd2253eee9393146c0f3c05d96ea
Janmejay Singh <singh.janmejay@gmail.com>
2025-09-24 00:55:55 +0530

    Settle into non-conflicting LSP keymap + add treesitter-context

--- lua/kickstart/plugins/lspconfig.lua
+++ lua/kickstart/plugins/lspconfig.lua
@@ -81,7 +81,7 @@ return {
           map('g/', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gR', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
