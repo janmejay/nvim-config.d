46c92838c2066a940f6b5c48b3c16ae0828a0c40
Janmejay Singh <singh.janmejay@gmail.com>
2025-09-27 14:55:10 +0530

    Get Copilot support working

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -1,15 +1,33 @@
 return {
   {
-    'zbirenbaum/copilot.lua',
-    cmd = 'Copilot',
-    event = 'InsertEnter', -- A good event for lazy-loading
-    opts = {},
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = {
+      suggestion = { enabled = false },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
   },
   {
-    'zbirenbaum/copilot-cmp',
-    after = { 'copilot.lua' },
-    config = function()
-      require('copilot_cmp').setup()
-    end,
+    "saghen/blink.cmp",
+    optional = true,
+    dependencies = { "fang2hou/blink-copilot" },
+    opts = {
+      sources = {
+        default = { "copilot" },
+        providers = {
+          copilot = {
+            name = "copilot",
+            module = "blink-copilot",
+            score_offset = 100,
+            async = true,
+          },
+        },
+      },
+    },
   },
 }
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -43,7 +43,7 @@ require('lazy').setup({
 
   require 'kickstart.plugins.treesitter',
   
-  -- require 'kickstart.plugins.copilot',
+  require 'kickstart.plugins.copilot',
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
@@ -55,7 +55,8 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.testing',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
